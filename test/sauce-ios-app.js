8a489216267807230fd5d021c74cf9d4a5a9202d
filmaj <maj.fil@gmail.com>
2015-09-17 12:28:44 -0700

    updating labeling and logging for the ios rdc test.

--- test/sauce-ios-app.js
+++ test/sauce-ios-app.js
@@ -15,17 +15,17 @@ chaiAsPromised.transferPromiseness = wd.transferPromiseness;
  * https://s3.amazonaws.com/sauce-misc/SampleCustomerApp.app.zip
  */
 
-let browser = wd.promiseChainRemote(creds.HOST, creds.PORT, creds.USER, creds.KEY);
+let app = wd.promiseChainRemote(creds.HOST, creds.PORT, creds.USER, creds.KEY);
 
-describe('On Sauce Labs, ', function() {
+describe('On Sauce Labs, on a real iOS device...', function() {
     this.timeout(240000);
     before(async () => {
         try {
-            await browser.init({
+            await app.init({
                 deviceName:'iPhone 6 Device',
                 platformName:'iOS',
                 platformVersion:'8.0',
-                app:'sauce-storage:SampleIOSApp',
+                app:'sauce-storage:SampleCustomerApp.app.zip',
                 name:'Real iOS device test w/ simple native app',
                 "appium-version":"1.4.7"
             });
@@ -33,20 +33,20 @@ describe('On Sauce Labs, ', function() {
             should.not.exist('Error connecting to Sauce Labs!', err);
         }
     });
-    it('should load google.com', async () => {
+    it('should load a simple iOS native app and switch views', async () => {
         try {
-            let button = await browser.elementByName('Second');
+            let button = await app.elementByName('Second');
             await button.click();
-            let view_text = await browser.elementByName('Second View');
+            let view_text = await app.elementByName('Second View');
             let is_text_visible = await view_text.isDisplayed();
-            console.log('Is second view visible? ', is_text_visible);
+            console.log('Is second view visible (this should say yes)? ', is_text_visible);
         } catch (err) {
             should.not.exist('Error during test!', err);
         }
     });
     after(async () => {
         try {
-            await browser.quit();
+            await app.quit();
         } catch(err) {
             should.not.exist('Error quitting?', err);
         }
